29236aaa40244acdb373006d6f17556db641f85e
Abd Elmalik Abd Elghafar <99559681+AbdElmalik100@users.noreply.github.com>
2025-07-26 20:15:45 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <main className='min-h-screen bg-Yellow grid place-items-center'>
-      <div className="box p-6 bg-white w-[384px] max-lg:px-4 max-lg:w-full shadow-[8px_8px_0px]  rounded-[20px] border border-black flex flex-col gap-3">
+      <div className="box p-6 bg-white w-[384px] max-lg:mx-4 max-lg:w-full shadow-[8px_8px_0px]  rounded-[20px] border border-black flex flex-col gap-3">
         <img className="rounded-[20px]" src="/images/illustration-article.svg" alt="Illustration Article" />
         <span className="badge rounded-sm mt-3 bg-Yellow text-black font-extrabold p-1 px-3 w-fit text-sm">Learning</span>
         <span className="font-medium">Published 21 Dec 2023</span>
